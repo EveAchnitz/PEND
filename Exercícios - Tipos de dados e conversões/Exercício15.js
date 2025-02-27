9515c560a910fd39a080515f495447cd3899cf7b
EveAchnitz <159180793+EveAchnitz@users.noreply.github.com>
2025-02-27 10:44:25 -0300

    Update Exercício15.js

--- Exercícios - Tipos de dados e conversões/Exercício15.js
+++ Exercícios - Tipos de dados e conversões/Exercício15.js
@@ -2,7 +2,7 @@ let ver = String(true);
 let fal = String(false);
 let nulo = String(null);
 let indef = String(undefined);
-let vazio = {};
+let vazio = String({});
 
 console.log(ver);// -> true
 console.log(fal);// -> false
