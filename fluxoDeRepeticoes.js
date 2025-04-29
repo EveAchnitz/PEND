62f1dbad59f103e8c8ece5d0fbeb6ef977bdf270
EveAchnitz <159180793+EveAchnitz@users.noreply.github.com>
2025-04-29 13:24:46 -0300

    Add files via upload

--- fluxoDeRepeticoes.js
+++ fluxoDeRepeticoes.js
@@ -4,7 +4,7 @@ function ex1(){
     for(let nome of nomes){
         console.log(nome);
     }
-}
+}//saída: Jorge Francisco Antônio Mário Maria
 
 function ex2(){
     let numeros = [45,55,18];
@@ -14,7 +14,7 @@ function ex2(){
         soma += num;
     }
     console.log(soma);
-}
+}//saída: 118
 
 function ex3(){
     let pessoa = {
@@ -26,20 +26,20 @@ function ex3(){
     for(let chave in pessoa){
         console.log(chave + ": " + pessoa[chave]);
     }
-}
+}//saída:nome: Viviane idade: 93 cidade: Dublin
 
 function ex4(){
     let endereco = {
         rua: "Rua do Ouvidor",
         bairro: "Jardim dos ouvidos",
         numero: 123
-    }
+    };
     let contador = 0;
     
     for(let propriedade in endereco){
         contador++;
     } console.log(contador);
-}
+}//saída: 3
 
 function ex5(){
     let nomes = ["Jorge","Francisco","Antônio","Mário","Maria"];
@@ -49,7 +49,7 @@ function ex5(){
         todos += nome + ", ";
     }
     console.log(todos);
-}
+}//saída: Jorge, Francisco, Antônio, Mário, Maria,
 
 function ex6(){
     let misto = [23, false,"aula"];
@@ -57,4 +57,22 @@ function ex6(){
     for(let elemento of misto){
         console.log(typeof elemento);
     }
-}
+}//saída: number boolean string
+
+function ex7(){
+    let pessoas = [{nome: "João", idade: 45},{nome: "Joana", idade: 23}];
+    
+    for(let chave in pessoas){
+        idade = pessoas[chave].idade + 1;
+        console.log(idade);
+    }
+}//saída: 46 24
+
+function ex8(){
+    let pessoa = {nome: "Roberta", idade:68};
+    let matrizPessoa = "";
+    
+    for(let chave in pessoa){
+        matrizPessoa += chave + ": " + pessoa[chave] + "; ";
+    } console.log(matrizPessoa);
+}//saída: nome: Roberta; idade: 68;
